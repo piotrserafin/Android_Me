0ff229c5146835bc227f49b796117d7f329b330e
Piotr Serafin <piotr.serafin.71@gmail.com>
2018-05-23 22:14:11 +0200

    Updated Gradle Plugin, Build Tools and Deps versions

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,8 +16,8 @@
 
 package com.example.android.android_me.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.android_me.R;
 
